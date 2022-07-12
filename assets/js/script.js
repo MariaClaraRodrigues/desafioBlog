2d3e79137b6d76c66f8bbf6ea0ec98ee4f419465
Maria Clara da Silva Rodrigues <mariarodrigues.dev@gmail.com>
2022-07-12 11:22:23 -0300

    Adicionando animação aos artigos do blog

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal('.artigoBlog', { delay: 200 });
+ScrollReveal().reveal('.data', { delay: 400 });
+ScrollReveal().reveal('.tituloBlog', { delay: 500 });
+ScrollReveal().reveal('.txtBlog', { delay: 600 });
